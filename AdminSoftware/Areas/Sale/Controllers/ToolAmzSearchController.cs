af70a3fc4af3fe28b876642178ec684eb0f05941
tung <tungnm06@gmail.com>
2020-10-12 01:49:34 +0700

    Update

---
+++ AdminSoftware/Areas/Sale/Controllers/ToolAmzSearchController.cs
@@ -0,0 +1,82 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.Mvc;
+
+namespace AdminSoftware.Areas.Sale.Controllers
+{
+    public class ToolAmzSearchController : Controller
+    {
+
+        [SaleFilter]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public class ElementCustom
+        {
+            //public IWebElement WebElement { get; set; }
+            public int Id;
+            public string UrlImage { get; set; }
+            public string Title { get; set; }
+
+        }
+
+
+        public JsonResult GetDataSearch(string keySearch, int? page = 1)
+        {
+
+            var listElm = new List<ElementCustom>();
+
+            if (string.IsNullOrEmpty(keySearch))
+            {
+                 return Json(listElm, JsonRequestBehavior.AllowGet);
+            }
+            ChromeOptions options = new ChromeOptions();
+            var path = HostingEnvironment.ApplicationHost.GetPhysicalPath() + "/Tool/jkompbllimaoekaogchhkmkdogpkhojg-3.0.2-Crx4Chrome.com.crx";
+            options.AddExtension(path);
+            ChromeDriver driver = new ChromeDriver(options);
+            //var test driver.FindElementByLinkText
+            var url = "https://www.amazon.com/s?k={0}&ref=nb_sb_noss";
+            url = string.Format(url, keySearch);
+            driver.Navigate().GoToUrl(url);
+
+            if (page != 1)
+            {
+                driver.FindElement(By.LinkText(page.ToString())).Click();
+
+            }
+            //var options = new ChromeOptions();
+            //options.AddExtension(@"D:\Test\jkompbllimaoekaogchhkmkdogpkhojg-3.0.2-Crx4Chrome.com.crx");
+            //var driver = new ChromeDriver(options);
+            // test
+
+            //var test = driver.FindElement(By.ClassName("a-pagination"));
+
+            var a = driver.FindElements(By.ClassName("s-include-content-margin"));
+            int i = 0;
+            foreach (var item in a)
+            {
+                ElementCustom elmCustom = new ElementCustom();
+                var elementRoot = item.FindElements(By.ClassName("xtaqv-root"));
+                if (elementRoot.Count > 0)
+                {
+                    i++;
+                    //elmCustom.WebElement = elementRoot[0];
+                    //var x = item.FindElement(By.ClassName("s-image"));
+                    elmCustom.Id = i;
+                    elmCustom.Title = item.FindElement(By.ClassName("a-size-base-plus")).Text;
+                    elmCustom.UrlImage = item.FindElement(By.ClassName("s-image")).GetAttribute("src");
+                    listElm.Add(elmCustom);
+                }
+            }
+            driver.Close();
+            return Json(listElm, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ AdminSoftware/Areas/Sale/JsAction/ToolAmzSearch/index.js
@@ -0,0 +1,134 @@
+﻿var record = 0;
+
+function Search() {
+    var keySearch = $("#TextSearch").val();
+    var page = $("#Page").data("kendoNumericTextBox").value();
+    var newDataSource = new kendo.data.DataSource({
+        transport: {
+            read: function (options) {
+                $.ajax(
+                    {
+                        type: 'POST',
+                        url: '/sale/ToolAmzSearch/GetDataSearch',
+                        dataType: "json",
+                        data: JSON.stringify({
+                            keySearch: keySearch,
+                            page : page
+                        }),
+                        contentType: 'application/json;charset=utf-8',
+                        success: function (response) {
+                            options.success(response);
+                        }
+                    });
+            }
+        },
+        schema: {
+            model: {
+                id: "Id"
+            }
+        },
+        pageSize: 9999,
+        serverPaging: false,
+        serverFiltering: false
+    });
+    var grid = $("#grdMain").data("kendoGrid");
+    grid.setDataSource(newDataSource);
+}
+
+$(document).ready(function () {
+    $("#Page").kendoNumericTextBox({
+        format: "{0:n0}",
+        min: 1,
+        step: 1
+    });
+
+    $("#btnSearchDate").click(function () {
+        Search()
+    });
+
+    $("#btnSearchDateLeft").click(function () {
+        var page = $("#Page").data("kendoNumericTextBox").value();
+
+        if (page == 1 || page == "1") {
+            return;
+        }
+        $("#Page").data("kendoNumericTextBox").value(page - 1);
+        Search()
+    });
+
+    $("#btnSearchDateRight").click(function () {
+        var page = $("#Page").data("kendoNumericTextBox").value();
+        $("#Page").data("kendoNumericTextBox").value(page + 1);
+        Search()
+    });
+
+
+    $("#grdMain").kendoGrid({
+        toolbar: [],
+        excel: {
+            fileName: "AmazoneRanking.xlsx",
+            filterable: true,
+            allPages: true
+        },
+        dataSource: {
+            transport: {
+                read: function (options) {
+                    $.ajax(
+                        {
+                            type: 'POST',
+                            url: '/sale/ToolAmzSearch/GetDataSearch',
+                            dataType: "json",
+                            data: JSON.stringify({
+                                keySearch : $("#TextSearch").val(),
+                                page: $("#Page").data("kendoNumericTextBox").value()
+                            }),
+                            contentType: 'application/json;charset=utf-8',
+                            success: function (response) {
+
+                                options.success(response);
+                            }
+                        });
+                }
+            },
+            schema: {
+                model: {
+                    id: "Id"
+                }
+            },
+            pageSize: 9999,
+            serverPaging: false,
+            serverFiltering: false
+        },
+        height: gridHeight,
+        filterable: true,
+        sortable: true,
+        pageable: {
+            refresh: true
+        },
+        selectable: 'row',
+        columns: [
+            {
+                title: "STT",
+                template: "#= ++record #",
+                width: 60
+            },
+            {
+                field: "Title",
+                title: "Tiêu đề sản phẩm",
+                width: 200
+            },
+            {
+                field: "UrlImage",
+                title: "Hình ảnh sản phẩm",
+                width: 500,
+                template: "#if(UrlImage != null){#" + "<a width='150' height='150' href='#=UrlImage#' target='_blank' style='text-align:center'><img width='150' height='150' src='#=UrlImage #' alt='image' /></a>" + "#}#",
+
+            }
+            //template: "#if(FilePath != null){#" + "<a href='#=FilePath#' target='_blank' style='text-align:center'><i class='glyphicon glyphicon-download'></i></a>" + "#}#",
+
+        ],
+        dataBinding: function () {
+            record = (this.dataSource.page() - 1) * this.dataSource.pageSize();
+        }
+    });
+});
